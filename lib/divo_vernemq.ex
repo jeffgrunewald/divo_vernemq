a366bd972ecfe17817c5ffc8d1500ea7e393a1fa
jeffgrunewald <jeff@grunewalddesign.com>
2020-04-10 18:26:46 -0400

    pushing default files to test image trigger isolation

---
+++ lib/divo_vernemq.ex
@@ -0,0 +1,18 @@
+defmodule DivoVernemq do
+  @moduledoc """
+  Documentation for DivoVernemq.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DivoVernemq.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/divo_vernemq_test.exs
@@ -0,0 +1,8 @@
+defmodule DivoVernemqTest do
+  use ExUnit.Case
+  doctest DivoVernemq
+
+  test "greets the world" do
+    assert DivoVernemq.hello() == :world
+  end
+end
